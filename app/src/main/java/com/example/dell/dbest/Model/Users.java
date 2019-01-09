e82e30a912fe718472e31dc1406774efd350c2b1
Seyi Emmanuel <seyi4guy@gmail.com>
2019-01-09 02:17:27 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dell/dbest/Model/Users.java
@@ -0,0 +1,45 @@
+package com.example.dell.dbest.Model;
+
+import java.io.File;
+
+public class Users {
+
+    private String name, phone, password;
+    public Users() {
+        super();
+    }
+
+    public Users(String name, String phone, String password) {
+        this.name = name;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public File getImage() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/dell/dbest/prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.dell.dbest.prevalent;
+
+import com.example.dell.dbest.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
